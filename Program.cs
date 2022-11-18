c8d26e86f2f254e20de7bfa96b1a222f977fe562
codychiles <94078061+codychiles@users.noreply.github.com>
2022-11-18 13:53:38 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace converthex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertHex("fortnite");
+        }
+        static void ConvertHex(string minecraft)
+        {
+
+            byte[] fortnite = Encoding.Default.GetBytes("sample");
+            var hexString = BitConverter.ToString(fortnite).ToLower();
+            hexString = hexString.Replace("-", " ");
+
+            Console.WriteLine("Hex value: " + hexString);
+        }
+    }
+}
